44c356c36e32c0f7fee1fef11281a44a0d64de5a
Vishwajeet <manevishwajeet1@gmail.com>
2024-07-13 23:13:55 +0200

    Adding User Page using toc-ui package

--- src/react-components/UsersPage.tsx
+++ src/react-components/UsersPage.tsx
@@ -4,97 +4,139 @@ import * as BUI from "@thatopen/ui"
 declare global {
   namespace JSX {
     interface IntrinsicElements {
-      'bim-grid': any;
+      "bim-grid": any;
     }
   }
 }
 
-const bimTable = BUI.Component.create<BUI.Table>(() => {
-  const onTableCreated = (element?: Element) => {
-    if (!element) return;
-    const table = element as BUI.Table;
-    table.data = [
-      {
-        data: {
-          name: "John Doe",
-          task: "Create Work Orders",
-          role: "Engineer",
-        },
-      },
-      {
-        data: {
-          name: "Jane Doe",
-          task: "Review Work Orders",
-          role: "Manager",
-        },
-      },
-    ]
-  }
-
-  return BUI.html `
-    <bim-table ${BUI.ref(onTableCreated)}></bim-table>
-  `
-})
-
-const contentPanel = BUI.Component.create<BUI.Panel>(() => {
-  return BUI.html `
-    <bim-panel style="border-radius: 0px">
-      <bim-panel-section label="Tasks">
-        ${bimTable}
-      </bim-panel-section>
-    </bim-panel> 
-  `;
-})
-
-const footerPanel = BUI.Component.create<BUI.Component>(() => {
-  const labelStyles = {
-    "color": "#ffffff",
-  }
-  return BUI.html `
-    <div style="display: flex; justify-content: center;">
-      <bim-label style=${BUI.styleMap(labelStyles)}>Copyright of That Contruction Company</bim-label>
-    </div>
-  `;
-})
-
-const sidebarPanel = BUI.Component.create<BUI.Component>(() => {
-  return BUI.html `
-    <div style="padding: 4px">
-      <bim-button label="Click Me"></bim-button>
-      <bim-button label="Export"></bim-button>
-    </div>
-  `;
-})
-
-export const gridLayout: BUI.Layouts = {
-  primary: {
-    template: `
-      "header header header" 40px
-      "contentPanel contentPanel sidebarPanel" 1fr
-      "footerPanel footerPanel footerPanel" 40px
-      / 1fr 1fr 60px
-    `,
-    elements: {
-      header: (() => {
-        const inputBox = BUI.Component.create<BUI.TextInput>(() => {
-          return BUI.html `
-            <bim-text-input style="padding: 8px" placeholder="Search"></bim-text-input>
-          `
-        })
-        return inputBox;
-      })(),
-      sidebarPanel,
-      contentPanel,
-      footerPanel,
-    },
-  }
-}
-
 export function UsersPage() {
+  const userTable = BUI.Component.create<BUI.Table>(() => {
+    const onTableCreated = (element?: Element) => {
+      if (!element) return;
+      const table = element as BUI.Table;
+      table.data = [
+        {
+          data: {
+            Name: "John Doe",
+            Task: "Create Work Orders",
+            Role: "Engineer",
+          },
+        },
+        {
+          data: {
+            Name: "Jane Doe",
+            Task: "Review Work Orders",
+            Role: "Manager",
+          },
+        },
+        {
+          data: {
+            Name: "Antonio",
+            Task: "Review PRs",
+            Role: "Developer",
+          },
+        },
+        {
+          data: {
+            Name: "Juan",
+            Task: "Create PRs",
+            Role: "Engineer",
+          },
+        },
+        {
+          data: {
+            Name: "Vishwajeet",
+            Task: "Content",
+            Role: "Developer",
+          },
+        }
+      ]
+    }
+  
+    return BUI.html `
+      <bim-table ${BUI.ref(onTableCreated)}></bim-table>
+    `
+  })
+  
+  const content = BUI.Component.create<BUI.Panel>(() => {
+    return BUI.html `
+      <bim-panel style="border-radius: 0px">
+        <bim-panel-section label="Tasks">
+          ${userTable}
+        </bim-panel-section>
+      </bim-panel> 
+    `;
+  })
+  
+  const footer = BUI.Component.create<BUI.Component>(() => {
+    const labelStyles = {
+      "color": "#ffffff",
+    }
+    return BUI.html `
+      <div style="display: flex; justify-content: center;">
+        <bim-label style=${BUI.styleMap(labelStyles)}>Copyright of That Contruction Company</bim-label>
+      </div>
+    `;
+  })
+  
+  const sidebar = BUI.Component.create<BUI.Component>(() => {
+    const buttonStyles = {
+      "font-size": "20px",
+      "height": "50px",
+    }
+  
+    return BUI.html `
+      <div style="padding: 4px">
+        <bim-button 
+          style=${BUI.styleMap(buttonStyles)} 
+          icon="fluent:dark-theme-20-regular"
+          @click=${() => {
+            console.log(userTable.value)
+          }}
+          ></bim-button>
+        <bim-button 
+          style=${BUI.styleMap(buttonStyles)} 
+          icon="uil:file-export"
+          @click=${() => {
+            const csvData = userTable.csv
+            const blob = new Blob([csvData], { type: "text/csv" })
+            const url = URL.createObjectURL(blob)
+            const a = document.createElement("a")
+            a.href = url
+            a.download = "users.csv"
+            a.click()
+          }}
+        ></bim-button>
+      </div>
+    `;
+  })
+  
+  const gridLayout: BUI.Layouts = {
+    primary: {
+      template: `
+        "header header header" 40px
+        "content content sidebar" 1fr
+        "footer footer footer" 40px
+        / 1fr 1fr 60px
+      `,
+      elements: {
+        header: (() => {
+          const inputBox = BUI.Component.create<BUI.TextInput>(() => {
+            return BUI.html `
+              <bim-text-input style="padding: 8px" placeholder="Search Something"></bim-text-input>
+            `
+          })
+          return inputBox;
+        })(),
+        sidebar,
+        content,
+        footer,
+      },
+    }
+  }
 
   React.useEffect (() => {
     BUI.Manager.init()
-
     const grid = document.getElementById("bimGrid") as BUI.Grid
     grid.layouts = gridLayout
     grid.layout = "primary"
