4cde47f7af21116024aba8ab892f04c51a37332a
Vishwajeet <manevishwajeet1@gmail.com>
2024-08-06 14:51:54 +0200

    changes in config

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,13 +1,13 @@
 import * as Firestore from "firebase/firestore";
-import { initializeApp } from "firebase/app";
+import { initializeApp } from "firebase/app"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC_8LGL4hcmkgsft84WXH_BVCgfAmHQzzU",
-  authDomain: "bim-dev-master-d1a5b.firebaseapp.com",
-  projectId: "bim-dev-master-d1a5b",
-  storageBucket: "bim-dev-master-d1a5b.appspot.com",
-  messagingSenderId: "614427913592",
-  appId: "1:614427913592:web:01985bb027ba89c8a0ac8d"
+  apiKey: "AIzaSyAkYexzjufLcJxGjQ4IBs0wHsKIpKQy9uM",
+  authDomain: "bim-dev-master-3edda.firebaseapp.com",
+  projectId: "bim-dev-master-3edda",
+  storageBucket: "bim-dev-master-3edda.appspot.com",
+  messagingSenderId: "924916602634",
+  appId: "1:924916602634:web:06123053d6eaa77ccd0cf3"
 };
 
 const app = initializeApp(firebaseConfig)
