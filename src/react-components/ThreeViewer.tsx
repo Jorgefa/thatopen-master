9a99aba2c10370058ec993df4c600a271b20a2e2
Vishwajeet <manevishwajeet1@gmail.com>
2024-09-09 23:21:36 +0200

    Final: M4-C3-L3

--- src/react-components/ThreeViewer.tsx
+++ src/react-components/ThreeViewer.tsx
@@ -4,7 +4,8 @@ import * as OBC from "@thatopen/components";
 
 export function ThreeViewer() {
   const setViewer = () => {
-    const viewer = new OBC.Components()
+    const viewer = new OBC.Components();
+
     const worlds = viewer.get(OBC.Worlds)
 
     const world = worlds.create<
@@ -17,28 +18,30 @@ export function ThreeViewer() {
     world.scene = sceneComponent
     world.scene.setup()
     world.scene.three.background = null
-  
+
     const viewerContainer = document.getElementById("viewer-container") as HTMLElement
     const rendererComponent = new OBC.SimpleRenderer(viewer, viewerContainer)
     world.renderer = rendererComponent
-    
+
     const cameraComponent = new OBC.OrthoPerspectiveCamera(viewer)
     world.camera = cameraComponent
-    
+
     viewer.init()
 
     const material = new THREE.MeshLambertMaterial({ color: "#6528D7" })
     const geometry = new THREE.BoxGeometry()
     const cube = new THREE.Mesh(geometry, material)
     world.scene.three.add(cube)
-
+    
     world.camera.controls.setLookAt(3, 3, 3, 0, 0, 0)
     world.camera.updateAspect()
   }
 
   React.useEffect(() => {
     setViewer();
-    return () => {};
+    return () => {
+      
+    };
   }, []);
 
   return (
