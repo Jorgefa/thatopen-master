6139cd0dc096d3c6f21ef5153de37b721d7aec9c
Vishwajeet <manevishwajeet1@gmail.com>
2024-09-19 15:55:52 +0200

    using components from base

--- src/bim-components/TodoCreator/src/TodoCreator.ts
+++ src/bim-components/TodoCreator/src/TodoCreator.ts
@@ -9,12 +9,10 @@ export interface TodoData {
 export class TodoCreator extends OBC.Component {
   static uuid = "f26555ec-4394-4349-986a-7409e4fd308e"
   enabled = true
-  private _components: OBC.Components
   onTodoCreated = new OBC.Event<TodoData>()
 
   constructor(components: OBC.Components) {
     super(components)
-    this._components = components
     components.add(TodoCreator.uuid, this)
   }
 
