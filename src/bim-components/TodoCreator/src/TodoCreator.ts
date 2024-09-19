bd2c2e1074fa38575abf3656dcde7f2141fa644b
Vishwajeet <manevishwajeet1@gmail.com>
2024-09-19 11:12:17 +0200

    using components fram base class

--- src/bim-components/TodoCreator/src/TodoCreator.ts
+++ src/bim-components/TodoCreator/src/TodoCreator.ts
@@ -3,11 +3,10 @@ import * as OBC from "@thatopen/components"
 export class TodoCreator extends OBC.Component {
   static uuid = "f26555ec-4394-4349-986a-7409e4fd308e"
   enabled = true
-  private _components: OBC.Components
 
   constructor(components: OBC.Components) {
     super(components)
-    this._components = components
+    this.components = components
     components.add(TodoCreator.uuid, this)
   }
 
