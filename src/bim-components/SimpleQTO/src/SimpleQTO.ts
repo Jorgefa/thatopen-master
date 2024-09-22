c7c7923f3a0954b61e58b50954f50273e99a7293
Vishwajeet <manevishwajeet1@gmail.com>
2024-09-22 20:58:09 +0200

    Lesson 2 Draft

--- src/bim-components/SimpleQTO/src/SimpleQTO.ts
+++ src/bim-components/SimpleQTO/src/SimpleQTO.ts
@@ -1,5 +1,6 @@
+import * as WEBIFC from "web-ifc"
 import * as OBC from "@thatopen/components"
-import { FragmentIdMap } from "@thatopen/fragments"
+import { FragmentIdMap, FragmentsGroup } from "@thatopen/fragments"
 
 export class SimpleQTO extends OBC.Component implements OBC.Disposable {
   static uuid = "3b5e8cea-9983-4bf6-b120-51152985b22d"
@@ -11,11 +12,32 @@ export class SimpleQTO extends OBC.Component implements OBC.Disposable {
     this.components.add(SimpleQTO.uuid, this)
   }
 
-  sumQuantities(fragmentIdMap: FragmentIdMap) {
+  setup() {
+
+  }
+
+  async sumQuantities(fragmentIdMap: FragmentIdMap) {
     const fragmentManager = this.components.get(OBC.FragmentsManager)
+    const indexer = this.components.get(OBC.IfcRelationsIndexer)
     for (const fragmentID in fragmentIdMap) {
       const fragment = fragmentManager.list.get(fragmentID)
-      console.log(fragment)
+      const model = fragment?.mesh.parent
+      if (!(model instanceof FragmentsGroup && model.hasProperties)) { continue }
+      const expressIDs = fragmentIdMap[fragmentID]
+      console.log(expressIDs)
+      for (const id of expressIDs) {
+        const psets = indexer.getEntityRelations(model, id, "IsDefinedBy")
+        if (psets) {
+          for (const expressId of psets) {
+            const prop = await model.getProperties(expressId)
+            // console.log(prop)
+            if(!prop) return
+            if(prop.type === WEBIFC.IFCELEMENTQUANTITY) {
+              console.log(prop)
+            }
+          }
+        }
+      }
     }
   }
 
