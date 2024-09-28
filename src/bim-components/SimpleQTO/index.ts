78ca0ba825d121aac27c70956a267e04363a32d5
Vishwajeet <manevishwajeet1@gmail.com>
2024-09-28 15:13:26 +0200

    remove comments

--- src/bim-components/SimpleQTO/index.ts
+++ src/bim-components/SimpleQTO/index.ts
@@ -4,13 +4,6 @@ import * as FRAGS from "@thatopen/fragments"
 
 type QtoResult = {[setName: string]: {[qtoName: string]: number}}
 
-// const sum = {
-//   Qto_WallBaseQuantities: {
-//     volume: 20,
-//     area: 30
-//   }
-// }
-
 export class SimpleQTO extends OBC.Component implements OBC.Disposable {
   static uuid = "3b5e8cea-9983-4bf6-b120-51152985b22d"
   enabled = true
@@ -33,20 +26,16 @@ export class SimpleQTO extends OBC.Component implements OBC.Disposable {
         model,
         WEBIFC.IFCRELDEFINESBYPROPERTIES,
         async (setID, relatedIDs) => {
+          const set = await model.getProperties(setID)
           const expressIDs = modelIdMap[modelId]
           const workingIDs = relatedIDs.filter(id => expressIDs.has(id))
-          const set = await model.getProperties(setID)
           const { name: setName } = await OBC.IfcPropertiesUtils.getEntityName(model, setID)
           if (set?.type !== WEBIFC.IFCELEMENTQUANTITY || workingIDs.length === 0 || !setName) { return }
-          // console.log(set)
-          // const expressIDs = modelIdMap[modelId]
-          // const workingIDs = relatedIDs.filter(id => expressIDs.has(id))
           if (!(setName in this._qtoResult)) { this._qtoResult[setName] = {} }
           await OBC.IfcPropertiesUtils.getQsetQuantities(
             model,
             setID,
             async (qtoID) => {
-              // console.log(await model.getProperties(qtoID))
               const { name: qtoName } = await OBC.IfcPropertiesUtils.getEntityName(model, qtoID)
               if (!qtoName) { return }
               if (!(qtoName in this._qtoResult[setName])) { this._qtoResult[setName][qtoName] = 0 }
