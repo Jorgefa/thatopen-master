d8ceaa20acf54d69cdae304ed9526c9101a85fbc
Jorge Alonso <jorge.alonso@pmgroup-global.com>
2024-10-06 18:34:37 +0200

    M3C3L5_Done

---
+++ src/react-components/ProjectCard.tsx
@@ -0,0 +1,42 @@
+import * as React from "react";
+
+export function ProjectCard() {
+  return (
+    <div className="project-card">
+        <div className="card-header">
+            <p
+            style={{
+                backgroundColor: "#ca8134",
+                padding: 10,
+                borderRadius: 8,
+                aspectRatio: 1
+            }}
+            >
+            HC
+            </p>
+            <div>
+            <h5>Project Name</h5>
+            <p>Project description</p>
+            </div>
+        </div>
+        <div className="card-content">
+            <div className="card-property">
+            <p style={{ color: "#969696" }}>Status</p>
+            <p>Active</p>
+            </div>
+            <div className="card-property">
+            <p style={{ color: "#969696" }}>Role</p>
+            <p>Developer</p>
+            </div>
+            <div className="card-property">
+            <p style={{ color: "#969696" }}>Cost</p>
+            <p>$1.000</p>
+            </div>
+            <div className="card-property">
+            <p style={{ color: "#969696" }}>Estimated Progress</p>
+            <p>67%</p>
+            </div>
+        </div>
+        </div>
+  )
+}
